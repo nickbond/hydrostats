9520d7c815ae03dc9ffde2844700cba2435a6fc0
Nick Bond <n.bond@griffith.edu.au>
2014-04-06 17:46:01 -0500

    changed return value from r to SD

--- R/day.dist.R
+++ R/day.dist.R
@@ -28,7 +28,7 @@ day.dist <- function(Dates=NULL, days=NULL, years=NULL) {
     sd.deg <- sd.rad * (365/(2 * pi))
     
     
-    #data.frame(mean.doy = round(m.deg, 0), sd.doy = round(sd.deg, 0))
+    #data.frame(mean.doy = round(m.deg, 0), sd.doy = round(1-r, 2))
     data.frame(mean.doy = round(m.deg, 0), sd.doy = round(sd.deg, 0))
     
 } 
