11ff15a70d0ffd61f47bd83ab7fb8dff979063aa
Nick Bond <n.bond@griffith.edu.au>
2014-06-23 18:28:30 +1000

    fixed the fact this was pasted in without a function prefix

--- R/high.spell.lengths.R
+++ R/high.spell.lengths.R
@@ -1,4 +1,4 @@
-function(flow.ts, quant = 0.9, user.threshold = F, defined.threshold, ind.days = 5, ignore.zeros = T, ctf.threshold = 0.1, hydro.year=FALSE) {
+high.spell.lengths<-function(flow.ts, quant = 0.9, user.threshold = F, defined.threshold, ind.days = 5, ignore.zeros = T, ctf.threshold = 0.1, hydro.year=FALSE) {
 	
 	if (hydro.year==TRUE) {
 		flow.ts <- hydro.year(flow.ts, hydro.year="hydro")
