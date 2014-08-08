caef606c90cd3fc64d20d5fe0c20575fa7d7f14a
Nick Bond <n.bond@griffith.edu.au>
2014-08-08 14:51:47 +1000

    roll back

--- R/four.digit.year.R
+++ R/four.digit.year.R
@@ -1,5 +1,5 @@
-four.digit.year <- function(x, change.year = 1968) {
-		n <- year(x)%%100
-    lubridate::year(x) <- ifelse(n > change.year%%100, 1900 + n, 2000 + n)
+four.digit.year <- function(x, year = 1968) {
+    n <- year(x)%%100
+    year(x) <- ifelse(n > year%%100, 1900 + n, 2000 + n)
     x
 } 
