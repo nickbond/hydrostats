226586a72d3d8c807b5c3d5444ea3dbda5ef5e25
Nick Bond <n.bond@griffith.edu.au>
2014-08-08 17:58:17 +1000

    ann.cv: removed need for ddply

--- R/ann.cv.R
+++ R/ann.cv.R
@@ -1,7 +1,8 @@
 ann.cv <- function(flow.ts) {
     Q <- NULL
     flow.ts$year <- strftime(flow.ts$Date, format = "%Y")
-    ann.stats <- ddply(flow.ts, "year", summarize, mean = mean(Q, na.rm = T), sd = sd(Q, na.rm = T))
+    ann.stats<-aggregate(. ~ year, flow.ts, function(x) c(mean = mean(x), sd = sd(x)))
+    #ann.stats <- ddply(flow.ts, 'year', summarize, mean = mean(Q, na.rm = T), sd = sd(Q, na.rm = T))
     
     out <- sd(ann.stats$mean, na.rm = T)/mean(ann.stats$mean, na.rm = T) * 100
     
