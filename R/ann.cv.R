18ed11d4da87dd0aa1bc89d174678ca6c6d25554
Nick Bond <n.bond@griffith.edu.au>
2014-08-11 08:24:25 +1000

    tidyR

--- R/ann.cv.R
+++ R/ann.cv.R
@@ -1,8 +1,8 @@
 ann.cv <- function(flow.ts) {
     Q <- NULL
     flow.ts$year <- strftime(flow.ts$Date, format = "%Y")
-    ann.stats<-aggregate(. ~ year, flow.ts, function(x) c(mean = mean(x), sd = sd(x)))
-    #ann.stats <- ddply(flow.ts, 'year', summarize, mean = mean(Q, na.rm = T), sd = sd(Q, na.rm = T))
+    ann.stats <- aggregate(. ~ year, flow.ts, function(x) c(mean = mean(x), sd = sd(x)))
+
     
     out <- sd(ann.stats$mean, na.rm = T)/mean(ann.stats$mean, na.rm = T) * 100
     
