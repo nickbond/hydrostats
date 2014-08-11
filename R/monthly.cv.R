087feed9fe870797131be9e7d077531007000bd0
Nick Bond <n.bond@griffith.edu.au>
2014-08-11 20:17:01 +1000

    fixed monthly cv calcs.

--- R/monthly.cv.R
+++ R/monthly.cv.R
@@ -1,6 +1,8 @@
 monthly.cv <- function(flow.ts) {
-    
-    monthly.cv <- (sd(flow.ts$Q, na.rm = T)/mean(flow.ts$Q, na.rm = T)) * 100
+		month.means <- aggregate(flow.ts[, "Q"], by = list(month = strftime(flow.ts[["Date"]], format = "%m")), mean, na.rm = T)
+		month.sd <- sd(month.means[,"x"], na.rm=T)
+		
+    monthly.cv <- (month.sd/mean(month.means[,"x"])) * 100
     
     return(data.frame(monthly.cv = monthly.cv))
 } 
