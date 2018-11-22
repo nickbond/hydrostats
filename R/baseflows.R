cebbca1703176b08cd3c8d6461e0c5624e1ec21d
nickbond <n.bond@latrobe.edu.au>
2018-11-22 10:31:10 +1000

    baseflows.R updated to convert Qf values less than 0 to 0, as per the formula in Ladson et al. 2013.

--- R/baseflows.R
+++ R/baseflows.R
@@ -32,13 +32,18 @@ baseflows <- function(flow.ts, a = 0.975, n.reflected = 30, ts = "mean") {
 		qf[1] <- (Q[2] - Q[1]) * ((1 + a)/2)
 		
 		for (i in 2:length(Q)) {
+			if (qf[i-1] <= 0) {
+				qf[i] <- 0
+			} else {
 			
 			qf[i] <- a * qf[i - 1] + ((Q[i] - Q[i - 1]) * ((1 + a)/2))
 			
+			}
 		}
+			
 		for (i in 1:length(Q)) {
 			
-			if (qf[i] < 0) {
+			if (qf[i] <= 0) {
 				qb[i] <- Q[i]
 			} else if (Q[i] > qf[i]) {
 				qb[i] <- Q[i] - qf[i]
